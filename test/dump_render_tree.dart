6ae45d73092fad4bd4947c426d90318774e88166
Richard Griffith <scribe.griff@gmail.com>
2013-06-10 16:40:48 -0500

    content_shell works on linux but not windows

--- test/dump_render_tree.dart
+++ test/dump_render_tree.dart
@@ -13,17 +13,9 @@ import 'package:unittest/unittest.dart';
 import 'dart:io';
 
 void main() {
-//  Process.run('DumpRenderTree', ['simplot_tests.html']).then((ProcessResult results) {
-//    if (results.exitCode == 0) {
-//      print('All tests completed successfully.');
-//    } else {
-//      print('Some of the tests have failed.');
-//    }
-//  });
-
-  // Once DRT is replaced with content_shell, the following is supposed to work.
-  // It currently returns with an exit code of 1.  The stderr is related to the
-  // Windows OS configuration.  Not sure how to fix that yet.
+  // The following passes on drone.io (ie, linux) but fails in Windows (returns
+  // exit code 1) when run locally.  The stderr is related to the Windows OS
+  // configuration.
   test('Simplot headless testing with content_shell.', () {
     var args = ['--dump-render-tree', 'simplot_tests.html'];
     expect(Process.run('content_shell', args).then((res) {
