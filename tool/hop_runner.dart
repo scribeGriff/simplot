cf4287f2d498c5d4fb59426e8395aa318045d471
Richard Griffith <scribe.griff@gmail.com>
2013-10-04 19:39:04 -0500

    see if have better luck with drone using hop

---
+++ tool/hop_runner.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2013, scribeGriff (Richard Griffith)
+// https://github.com/scribeGriff/simplot
+// All rights reserved.  Please see the LICENSE.md file.
+
+library dumprendertree;
+
+/**
+ * DumpRenderTree process for headless testing.
+ *
+ */
+
+import 'package:hop/hop.dart';
+import 'dart:io';
+import 'dart:async';
+
+main() {
+  addTask('test', createUnitTestTask());
+  runHop();
+}
+
+Task createUnitTestTask() {
+  final allPassedRegExp = new RegExp('All \\d+ tests passed');
+  return new Task.sync((TaskContext tcontext) {
+    tcontext.info("Running Unit Tests....");
+    var result = Process.run('content_shell',
+        ['--dump-render-tree','test/simplot_tests.html'])
+        .then((ProcessResult process) {
+          tcontext.info(process.stdout);
+          return allPassedRegExp.hasMatch(process.stdout);
+        });
+    return result;
+  });
+}
